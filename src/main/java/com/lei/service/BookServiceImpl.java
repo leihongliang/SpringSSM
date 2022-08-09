fdcbc9d76690737a01128ced2f85b190b79c793f
leihongliang <1204062841@qq.com>
2022-08-09 16:22:11 +0800

    Initial commit

---
+++ src/main/java/com/lei/service/BookServiceImpl.java
@@ -0,0 +1,45 @@
+package com.lei.service;
+import com.lei.dao.BookMapper;
+import com.lei.pojo.Books;
+import java.util.List;
+//@Service
+public class BookServiceImpl implements BookService {
+    /**
+     * service调dao层，组合Dao
+     * spring横切
+     */
+    //@Autowired
+    private BookMapper bookMapper;
+
+    public void setBookMapper(BookMapper bookMapper) {
+        this.bookMapper = bookMapper;
+    }
+
+    public int addBook(Books books) {
+        return bookMapper.addBook(books);
+    }
+
+    public int deleteBookById(int id) {
+        return bookMapper.deleteBookById(id);
+    }
+
+    public int updateBook(Books books) {
+        return bookMapper.updateBook(books);
+    }
+
+    public Books queryBookById(int id) {
+        return bookMapper.queryBookById(id);
+    }
+
+    public List<Books> queryAllBooks() {
+        return bookMapper.queryAllBooks();
+    }
+
+    public List<Books> queryBook(String bookName) {
+        return bookMapper.queryBook(bookName);
+    }
+    public Books queryBookByName(String bookName) {
+        return bookMapper.queryBookByName(bookName);
+    }
+}
+
